cd702efcbd9e4906bf5a3e1b331566680cb5c1d5
Blago <blagoy.stoitsov@gmail.com>
2021-11-01 17:01:24 +0200

    Initial commit

---
+++ client/src/components/Header/NavigationItem/NavigationItem.js
@@ -0,0 +1,10 @@
+import "./NavigationItem.css";
+const NavigationItem = (props) => {
+  return (
+    <li className="listItem">
+      <a href="#">{props.children}</a>
+    </li>
+  );
+};
+
+export default NavigationItem;

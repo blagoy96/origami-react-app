67bb72641002c0b867548ae11975e465f4a96871
blagoy96 <blagoy.stoitsov@gmail.com>
2021-11-03 14:49:58 +0200

    Updates on css

--- client/src/components/Main/Menu/Menu.js
+++ client/src/components/Main/Menu/Menu.js
@@ -4,17 +4,17 @@ const Menu = () => {
   return (
     <aside className="menu">
       <ul>
-        <MenuItem></MenuItem>
-        <MenuItem></MenuItem>
-        <MenuItem></MenuItem>
-        <MenuItem></MenuItem>
-        <MenuItem></MenuItem>
-        <MenuItem></MenuItem>
-        <MenuItem></MenuItem>
-        <MenuItem></MenuItem>
-        <MenuItem></MenuItem>
-        <MenuItem></MenuItem>
-        <MenuItem></MenuItem>
+        <MenuItem>Going to 1</MenuItem>
+        <MenuItem>Going to 2</MenuItem>
+        <MenuItem>Going to 3</MenuItem>
+        <MenuItem>Going to 4</MenuItem>
+        <MenuItem>Going to 5</MenuItem>
+        <MenuItem>Going to 6</MenuItem>
+        <MenuItem>Going to 7</MenuItem>
+        <MenuItem>Going to 8</MenuItem>
+        <MenuItem>Going to 9</MenuItem>
+        <MenuItem>Going to 10</MenuItem>
+        <MenuItem>Going to 11</MenuItem>
       </ul>
     </aside>
   );
